cde35959387e20e07cc62812c9eefcd40217d1da
fuZi9n <62058859+fuZi9n@users.noreply.github.com>
2023-03-29 13:33:54 +0200

    minor updates

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+	// Database credentials
+	define('DB_SERVER', '38.242.128.50');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', 'Kode1234!');
+	define('DB_NAME', 'login_system');
+
+	// Attempt to connect to MySQL database
+	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+	if (!$mysql_db) {
+		die("Error: Unable to connect " . $mysql_db->connect_error);
+	}
